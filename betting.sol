a15d56ec2f23cc20b74b5071d628da0baf71b938
Ashwin Ganesh <16601451+aganesh10@users.noreply.github.com>
2021-03-18 17:58:54 -0700

    Updated constructor of contract

--- betting.sol
+++ betting.sol
@@ -6,34 +6,35 @@ contract Bet {
     struct Player {
         address _address; 
         uint amount;
-        bytes32 teamName;
+        uint team;
     }
 
-    struct Team {
-        bytes32 name;
-        address[] bettors;
+    struct Game {
+        uint total1;
+        uint total2;
     }
-    
-    
-    mapping(address => Player) public allBettors;
 
-
-    constructor(uint amount, bytes32 teamName) {
+    mapping(address => Player) public team1;
+    mapping(address => Player) public team2;
+    
+    constructor(uint amount, uint team) {
         Player memory leader = Player({
             _address: msg.sender,
             amount: amount,
-            teamName: teamName
+            team: team
         });
-        
-        allBettors[msg.sender] = leader;
-        address[] memory teamBettors = new address[](1);
-        
-        Team memory team = Team({
-            name: leader.teamName,
-            bettors: teamBettors
-            
+
+        Game memory game = Game({
+            total1: 0,
+            total2: 0
         });
-        
-        team.bettors.push(leader._address);
+
+        if (leader.team == 1) {
+            game.total1 += amount;
+            leader = team1[leader._address];
+        } else {
+            game.total2 += amount;
+            leader = team2[leader._address];
+        }
     }
 }
