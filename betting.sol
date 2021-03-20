fca27df29cff309d39270c655f36050811729fa8
ayush0624 <31373700+ayush0624@users.noreply.github.com>
2021-03-20 18:34:08 -0400

    added join game function

--- betting.sol
+++ betting.sol
@@ -7,6 +7,7 @@ contract Bet {
         address _address; 
         uint amount;
         uint team;
+        bool exists;
     }
 
     struct Game {
@@ -16,15 +17,17 @@ contract Bet {
 
     mapping(address => Player) public team1;
     mapping(address => Player) public team2;
+    Game public game; //Public for testing purposes
     
     constructor(uint amount, uint team) {
         Player memory leader = Player({
             _address: msg.sender,
             amount: amount,
-            team: team
+            team: team,
+            exists: true
         });
 
-        Game memory game = Game({
+        game = Game({
             total1: 0,
             total2: 0
         });
@@ -35,6 +38,29 @@ contract Bet {
         } else {
             game.total2 += amount;
             leader = team2[leader._address];
+        }
+    }
+    
+    function playerExists(address _address) internal view returns(bool) {
+        return team1[_address].exists || team2[_address].exists;
+    }
+    
+    function joinGame(uint team) public payable {
+        require(!playerExists(msg.sender));
+        require(msg.value > 0);
+        
+        Player memory bettor = Player({
+            _address: msg.sender,
+            amount: msg.value,
+            team: team,
+            exists: true});
+        
+        if (team == 1) {
+            game.total1 += msg.value;
+            team1[msg.sender] = bettor;
+        } else {
+            game.total2 += msg.value;
+            team2[msg.sender] = bettor;
         }
     }
 }
