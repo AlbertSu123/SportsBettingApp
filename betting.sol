0c516561295f3ef02645fe3729b417f24f96e8a7
Ashwin Ganesh <16601451+aganesh10@users.noreply.github.com>
2021-03-20 22:51:57 -0700

    Rough draft of payout function

--- betting.sol
+++ betting.sol
@@ -5,26 +5,30 @@ pragma solidity >=0.7.0 <0.8.0;
 contract Bet {
     struct Player {
         address _address; 
-        uint amount;
-        uint team;
+        uint16 amount;
+        uint8 team;
     }
 
     struct Game {
-        uint total1;
-        uint total2;
+        uint16 total1;
+        uint16 total2;
     }
 
     mapping(address => Player) public team1;
+    address[] public team1Keys;
     mapping(address => Player) public team2;
-    
-    constructor(uint amount, uint team) {
+    address[] public team2Keys;
+
+    Game public game;
+
+    constructor(uint16 amount, uint8 team) {
         Player memory leader = Player({
             _address: msg.sender,
             amount: amount,
             team: team
         });
 
-        Game memory game = Game({
+        game = Game({
             total1: 0,
             total2: 0
         });
@@ -32,9 +36,42 @@ contract Bet {
         if (leader.team == 1) {
             game.total1 += amount;
             leader = team1[leader._address];
+            team1Keys.push(leader._address);
         } else {
             game.total2 += amount;
             leader = team2[leader._address];
+            team2Keys.push(leader._address);
+        }
+    }
+
+
+    function payout(uint8 winner) public payable{ 
+        uint16 amount;
+        if (winner == 1) {
+            uint16 ratio1 = game.total2 / game.total1;
+            for (uint8 i = 0; i < team1Keys.length; i++) {
+                Player memory curr = team1[team1Keys[i]];
+                amount = curr.amount + curr.amount * ratio1;
+                payable(team1Keys[i]).transfer(amount);
+            }
+        }
+        else if (winner == 2) {
+            uint16 ratio2 = game.total1 / game.total2;
+            for (uint8 i = 0; i < team2Keys.length; i++) {
+                Player memory curr = team2[team2Keys[i]];
+                amount = curr.amount + curr.amount * ratio2;
+                payable(team2Keys[i]).transfer(amount);
+            }
+        }
+        else {
+            for (uint8 i = 0; i < team1Keys.length; i++) {
+                Player memory curr = team1[team1Keys[i]];
+                payable(team1Keys[i]).transfer(curr.amount);
+            }
+            for (uint8 i = 0; i < team2Keys.length; i++) {
+                Player memory curr = team2[team2Keys[i]];
+                payable(team2Keys[i]).transfer(curr.amount);
+            }
         }
     }
 }
