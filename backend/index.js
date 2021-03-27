dc487213233da51bfee8bc00058b74758cdbbc53
ian <iac@berkeley.edu>
2021-03-26 17:26:44 -0700

    Added week 6 queries & init table queries

--- backend/index.js
+++ backend/index.js
@@ -8,33 +8,152 @@ var connection = mysql.createConnection({
 	database: "main"
 });
 
-connection.connect(function(err) {
-	if (err) {
-		console.error("database connection failed: " + err.stack);
-		return;
-	}
-	console.log("connected to database");
-
-	var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 51 Revisited')";
-
-	connection.query(sql, function (err, result) {
-		if (err) {
-			console.error("database connection failed: " + err.stack);
-			return;
-		}
-		console.log("1 record inserted");
-	});
-
-	connection.query( "SELECT * FROM customers", function (err, result, fields) {
-		if (err) {
-			console.error("database connection failed: " + err.stack);
-			return;
-		}
-		console.log(result);
-	});
-
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
 });
 
-//connection.end();
+rl.question("Create new user (newuser), Create new bet round (newround), Attach bet (attachbet)? ", function(options) {
+	if (options == 'newuser'){
+		rl.question("What is your first name? ", function(first_name) {
+			rl.question("What is your last name? ", function(last_name) {
+				rl.question("What is your email address? ", function(email) {
+					rl.question("What is your metamask address? ", function(metamask) {
+						rl.close();
+						connection.connect(function(err) {
+							if (err) {
+								console.error("database connection failed: " + err.stack);
+								return;
+							}
+							console.log("connected to database");
+							
+							var sql = `INSERT INTO users (first_name, last_name, email, metamask_address) VALUES ('${first_name}', '${last_name}', '${email}', '${metamask}')`;
+						
+							connection.query(sql, function (err, result) {
+								if (err) {
+									console.error("database connection failed: " + err.stack);
+									return;
+								}
+								console.log("1 record inserted");
+							});
+						
+							connection.query( "SELECT * FROM users", function (err, result, fields) {
+								if (err) {
+									console.error("database connection failed: " + err.stack);
+									return;
+								}
+								console.log(result);
+							});
+						
+							connection.end();
+						});
+						
+					});
+				});
+			});
+		});
+	} else if (options == 'newround') {
+		rl.question("What is team 1 name? ", function(team1_name) {
+			rl.question("What is team 2 name? ", function(team2_name) {
+				rl.close();
+				connection.connect(function(err) {
+					if (err) {
+						console.error("database connection failed: " + err.stack);
+						return;
+					}
+					console.log("connected to database");
+					
+					var sql = `INSERT INTO betting_rounds (team1, team2, team1_total, team2_total) VALUES ('${team1_name}', '${team2_name}', '0', '0')`;
+				
+					connection.query(sql, function (err, result) {
+						if (err) {
+							console.error("database connection failed: " + err.stack);
+							return;
+						}
+						console.log("1 record inserted");
+					});
+				
+					connection.query( "SELECT * FROM betting_rounds", function (err, result, fields) {
+						if (err) {
+							console.error("database connection failed: " + err.stack);
+							return;
+						}
+						console.log(result);
+					});
+				
+					connection.end();
+				});
+			});
+		});
+	} else if (options == 'attachbet') {
+		rl.question("What is your user id? ", function(userid) {
+			rl.question("What is the betting id? ", function(roundid) {
+				rl.question("What team do you bet on (team1 or team2)? ", function(teamid) {
+					if (teamid == "team1" || teamid == "team2") {
+						rl.question("How much do you bet? ", function(amount) {
+							rl.close();
+							connection.connect(function(err) {
+								if (err) {
+									console.error("database connection failed: " + err.stack);
+									return;
+								}
+								console.log("connected to database");
+								
+								var sql = `UPDATE betting_rounds SET ${teamid}_total = ${teamid}_total + ${amount} WHERE roundid=${roundid}`;
+								var sql1 = `INSERT INTO user_bets (userid, roundid, team_choice, bet_total) VALUES ('${userid}', '${roundid}', '${teamid}', '${amount}')`;
+
+								connection.query(sql, function (err, result) {
+									if (err) {
+										console.error("database write failed: " + err.stack);
+										return;
+									}
+									console.log("1 record inserted");
+								});
+							
+								connection.query(sql1, function (err, result) {
+									if (err) {
+										console.error("database write failed: " + err.stack);
+										return;
+									}
+									console.log("1 record inserted");
+								});
+
+								connection.query( "SELECT * FROM betting_rounds", function (err, result, fields) {
+									if (err) {
+										console.error("database connection failed: " + err.stack);
+										return;
+									}
+									console.log(result);
+								});
+							
+								connection.query( "SELECT * FROM user_bets", function (err, result, fields) {
+									if (err) {
+										console.error("database connection failed: " + err.stack);
+										return;
+									}
+									console.log(result);
+								});
+
+								connection.end();
+							});
+						});
+					} else {
+						console.log("not valid user! Please select team1 or team2");
+						rl.close();
+					}
+				});
+			});
+		});
+	} else {
+		console.log("not valid option!");
+		rl.close();
+	}
+	
+});
+
+
+
+//
 
 
---
+++ backend/queries.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE main;
+create table users (
+   userid INT NOT NULL AUTO_INCREMENT,
+   first_name VARCHAR(25) NOT NULL,
+   last_name VARCHAR(25) NOT NULL,
+   email VARCHAR(55) NOT NULL,
+   metamask_address VARCHAR(255) NOT NULL,
+   PRIMARY KEY ( userid )
+);
+create table betting_rounds (
+   roundid INT NOT NULL AUTO_INCREMENT,
+   team1 VARCHAR(25) NOT NULL,
+   team2 VARCHAR(25) NOT NULL,
+   team1_total FLOAT(10, 2) NOT NULL,
+   team2_total FLOAT(10, 2) NOT NULL,
+   PRIMARY KEY ( roundid )
+);
+create table user_bets (
+   userid INT NOT NULL,
+   roundid INT NOT NULL,
+   team_choice VARCHAR(25) NOT NULL,
+   bet_total FLOAT(10, 2) NOT NULL
+);
