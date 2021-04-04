208c0017df5b0719b20bee3f279dec32ccd00592
Ashwin Ganesh <16601451+aganesh10@users.noreply.github.com>
2021-04-04 11:27:52 -0700

    Moved oracle stuff to seperate file and made imovements to betting.sol

--- blockchain/contracts/betting.sol
+++ blockchain/contracts/betting.sol
@@ -1,162 +1,110 @@
-// SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >0.5.0;
 
-pragma solidity >= 0.5.0 < 0.6.0;
-import "github.com/provable-things/ethereum-api/provableAPI_0.5.sol";
-
-
-contract Bet is usingProvable {
+contract Bet { 
+    uint256 public minimumBet;
+    uint256 public total1;
+    uint256 public total2;
+    Player public leader;
+    address payable public leader_addy;
+    
+    mapping(address => Player) public team1;
+    address payable[] public addy1;
+    mapping(address => Player) public team2;
+    address payable[] public addy2;
+  
     struct Player {
-        address _address; 
         uint256 amount;
         uint32 team;
-        bool exists;
     }
 
-    struct Game {
-        uint256 total1;
-        uint256 total2;
-    }
-
-    mapping(address => Player) public team1;
-    address[] public team1Keys;
-    mapping(address => Player) public team2;
-
-    address[] public team2Keys;
-
-    Game public game;
-    Player public leader;
-    string public winner;
-    string team1String; 
-    string team2String;
-    string gameID;
-    
-    event LogNewProvableQuery(string description);
-    event LogResult(string result);
-    
-
-    constructor(uint256 _amount, uint32 _team, string memory _gameID) public {
+    constructor(uint256 _amount, uint32 _team) {
         leader = Player({
-            _address: msg.sender,
             amount: _amount,
-            team: _team,
-            exists: true
+            team: _team
         });
 
-        game = Game({
-            total1: 0,
-            total2: 0
-        });
+        total1 = 0;
+        total2 = 0;
+        minimumBet = 100000000000000; //Figure out actual minimum bet
+        leader_addy = msg.sender;
 
         if (leader.team == 1) {
-            game.total1 += leader.amount;
-            team1[leader._address] = leader;
-            team1Keys.push(leader._address);
+            total1 += leader.amount;
+            team1[leader_addy] = leader;
+            addy1.push(leader_addy);
         } else {
-            game.total2 += leader.amount;
-            team2[leader._address] = leader;
-            team2Keys.push(leader._address);
-        }
-        
-       
-        gameID = _gameID;
-    }
-    
-    function __callback(
-        bytes32 _myid,
-        string memory _result
-    )
-        public
-    {
-        require(msg.sender == provable_cbAddress());
-        emit LogResult(_result);
-        winner = _result;
-    }
-
-     function request(
-        string memory _query,
-        string memory _method,
-        string memory _url,
-        string memory _kwargs
-    )
-        public
-        payable
-    {
-        if (provable_getPrice("computation") > address(this).balance) {
-            emit LogNewProvableQuery("Provable query was NOT sent, please add some ETH to cover for the query fee");
-        } else {
-            emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
-            provable_query("computation",
-                [_query,
-                _method,
-                _url,
-                _kwargs]
-            );
+            total2 += leader.amount;
+            team2[leader_addy] = leader;
+            addy2.push(leader_addy);
         }
     }
-    
-    function requestCustomHeaders()
-        public
-        payable
-    {
-        request("json(QmdKK319Veha83h6AYgQqhx9YRsJ9MJE7y33oCXyZ4MqHE)",
-                "GET",
-                "https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=basketball",
-                "{'headers': {'x-rapidapi-key': '78ea5e7bfdmshae8e2447cc43203p1d9630jsn00e54f133806', 'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'}}");
-    }
 
-
-    function payout(uint32 winner) public payable{ 
-        uint256 amount;
-        if (winner == 1) {
-            uint256 ratio1 = game.total2 / game.total1;
-            for (uint32 i = 0; i < team1Keys.length; i++) {
-                Player memory curr = team1[team1Keys[i]];
-                amount = curr.amount + curr.amount * ratio1;
-                address(uint160(team1Keys[i])).transfer(amount);
+    function playerExists(address _addy) internal view returns(bool) {
+        for (uint8 i = 0; i < addy1.length; i++) {
+            if (addy1[i] == _addy) {
+                return true;
             }
         }
-        else if (winner == 2) {
-            uint256 ratio2 = game.total1 / game.total2;
-            for (uint32 i = 0; i < team2Keys.length; i++) {
-                Player memory curr = team2[team2Keys[i]];
+        for (uint8 i = 0; i < addy2.length; i++) {
+            if (addy2[i] == _addy) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function kill() public {
+        if (msg.sender == leader_addy) selfdestruct(leader_addy);
+    }
+
+    function joinGame(uint32 team) public payable {
+        require(!playerExists(msg.sender));
+        require(msg.value > minimumBet);
+        
+        Player memory bettor = Player({
+            amount: msg.value,
+            team: team});
+        
+        if (team == 1) {
+            total1 += msg.value;
+            team1[msg.sender] = bettor;
+            addy1.push(msg.sender);
+        } else {
+            total2 += msg.value;
+            team2[msg.sender] = bettor;
+            addy2.push(msg.sender);
+        }
+    }
+
+    
+    function payout(uint32 _winner) public payable{ 
+        uint256 amount;
+        if (_winner == 1) {
+            uint256 ratio1 = total2 / total1;
+            for (uint32 i = 0; i < addy1.length; i++) {
+                Player memory curr = team1[addy1[i]];
+                amount = curr.amount + curr.amount * ratio1;
+               addy1[i].transfer(amount);
+            }
+        }
+        else if (_winner == 2) {
+            uint256 ratio2 = total1 / total2;
+            for (uint32 i = 0; i < addy2.length; i++) {
+                Player memory curr = team2[addy2[i]];
                 amount = curr.amount + curr.amount * ratio2;
-                address(uint160(team2Keys[i])).transfer(amount);
+                addy2[i].transfer(amount);
             }
         }
         else {
-            for (uint8 i = 0; i < team1Keys.length; i++) {
-                Player memory curr = team1[team1Keys[i]];
-                address(uint160(team1Keys[i])).transfer(curr.amount);
+            for (uint8 i = 0; i < addy1.length; i++) {
+                Player memory curr = team1[addy1[i]];
+                addy1[i].transfer(curr.amount);
             }
-            for (uint8 i = 0; i < team2Keys.length; i++) {
-                Player memory curr = team2[team2Keys[i]];
-                address(uint160(team2Keys[i])).transfer(curr.amount);
+            for (uint8 i = 0; i < addy2.length; i++) {
+                Player memory curr = team2[addy2[i]];
+               addy2[i].transfer(curr.amount);
             }
-        }
-    }
-    
-    function playerExists(address _address) internal view returns(bool) {
-        return team1[_address].exists || team2[_address].exists;
-    }
-    
-    function joinGame(uint32 team) public payable {
-        require(!playerExists(msg.sender));
-        require(msg.value > 0);
-        
-        Player memory bettor = Player({
-            _address: msg.sender,
-            amount: msg.value,
-            team: team,
-            exists: true});
-        
-        if (team == 1) {
-            game.total1 += msg.value;
-            team1[bettor._address] = bettor;
-            team1Keys.push(bettor._address);
-        } else {
-            game.total2 += msg.value;
-            team2[bettor._address] = bettor;
-            team2Keys.push(bettor._address);
         }
     }
 }
